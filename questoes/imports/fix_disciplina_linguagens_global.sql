0f849728d096c3cc07c2f79f7d2354f474ce9cd9
João Pedro Delgado <delgadojoaopedro01@gmail.com>
2026-08-23 01:21:35 -0300

    Small changes to aesthetics

--- questoes/imports/fix_disciplina_linguagens_global.sql
+++
@@ -1,7 +0,0 @@
--- Unifica qualquer questao do banco (nao so as do ENEM 2009) que ainda esteja
--- com disciplina em portugues/literatura/artes para a disciplina unica
--- 'linguagens', alinhado com a nova convencao usada em todo o site
--- (gerar-lista, buscar, calendario, flashcards, simulados, dashboard geral).
-UPDATE questions
-SET disciplina = 'linguagens'
-WHERE disciplina IN ('portugues', 'literatura', 'artes');
